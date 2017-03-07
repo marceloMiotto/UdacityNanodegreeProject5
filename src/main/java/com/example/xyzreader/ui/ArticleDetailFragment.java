3b420128c0ec108673e9e51246a9fd65c7929fb0
Marcelo_Miotto <mmiotto@gmail.com>
2017-03-07 14:32:43 -0300

    Udacity Code Review I

--- src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -210,7 +210,7 @@ public class ArticleDetailFragment extends Fragment implements
                                 Palette p = Palette.generate(bitmap, 12);
                                 mMutedColor = p.getDarkMutedColor(0xFF333333);
                                 mPhotoView.setImageBitmap(bitmap);
-                                mPhotoView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                                mPhotoView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                                 updateStatusBar();
                             }
                         }
